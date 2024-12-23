8cfbbe10c1d3656277681325e002d61bd16cc2cf
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-12-24 00:44:21 +0530

    index.js file update

--- backend/index.js
+++ backend/index.js
@@ -6,17 +6,18 @@ const helmet = require('helmet');
 const propertyRoutes = require('./routes/propertyRoutes');
 const connectRoutes = require('./routes/connectRoutes');
 const wifiRoutes = require('./routes/wifiRoutes');
-const functions = require("firebase-functions")
+const functions = require("firebase-functions");
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
+// Security middleware
 app.use(helmet());
 app.use(express.json());
 
-// Configure CORS
-const allowedOrigins = ['http://127.0.0.1:5500','https://linkbase.tech'];
+// Configure CORS with allowed origins
+const allowedOrigins = ['http://127.0.0.1:5500', 'https://linkbase.tech'];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -32,12 +33,13 @@ app.use(cors({
 app.get("/", (req, res) => res.send("Hello from Firebase!"));
 app.use('/propertiesDetails', propertyRoutes);
 app.use('/userConnect', connectRoutes);
-app.use('/connect/external',wifiRoutes);
+app.use('/connect/external', wifiRoutes);
 
-// For Local Testing API
+// Uncomment the following for local testing (when not deploying to Firebase)
 // const port = process.env.PORT || 8080;
 // app.listen(port, () => {
 //   console.log(`Server is running on port ${port}`);
 // });
 
+// Firebase Function export
 exports.api = functions.https.onRequest(app);
