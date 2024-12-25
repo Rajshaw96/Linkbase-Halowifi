c85104d8145d34d98173949c24c78303c6ae5da7
Rajkumar Verma <rajkumarshaw624@gmail.com>
2024-12-25 17:59:21 +0530

    code update

--- backend/index.js
+++ backend/index.js
@@ -17,22 +17,22 @@ app.use(helmet());
 app.use(express.json());
 
 // Configure CORS with allowed origins
-const allowedOrigins = [
-  'http://127.0.0.1:5500', 
-  'https://linkbase.tech',
-  'https://linkbase.tech/', 
-  'https://linkbase-halowifi.web.app'
-];
+// const allowedOrigins = [
+//   'http://127.0.0.1:5500', 
+//   'https://linkbase.tech',
+//   'https://linkbase.tech/', 
+//   'https://linkbase-halowifi.web.app'
+// ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
 
 // Test Route
 app.get("/", (req, res) => res.send("Hello from Firebase!"));
