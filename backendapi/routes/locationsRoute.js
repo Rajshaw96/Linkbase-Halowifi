f59593040509deac6c19a8db81a61c9f5618ed95
Rajkumar Verma <rajkumarshaw624@gmail.com>
2025-01-04 14:14:05 +0530

    fix error

--- backendapi/routes/locationsRoute.js
+++ backendapi/routes/locationsRoute.js
@@ -15,7 +15,7 @@ router.get("/getAllLocations", async (req, res) => {
       });
     }
 
-    const response = await axios.get(`${process.env.EXTERNAL_API_URL}/api/locations`, {
+    const response = await axios.get(`${process.env.EXTERNAL_API_URL}/api/external/locations`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -82,7 +82,7 @@ router.get("/getAllLocations/:id", async (req, res) => {
       });
     }
 
-    const response = await axios.get(`${process.env.EXTERNAL_API_URL}/api/locations/${id}`, {
+    const response = await axios.get(`${process.env.EXTERNAL_API_URL}/api/external/locations/${id}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
